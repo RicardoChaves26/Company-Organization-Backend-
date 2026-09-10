63887aedb025b04ef2f6f498ffddaf886b995d9c
Ricardo Chaves Campos <richavesca@est.utn.ac.cr>
2026-09-10 13:11:03 -0600

    fix: Se actualizo el service
    - El service ahora nos permite encriptar las contraseñas.
    - Y se descargo la dependencia necesaria.

--- src/services/usuario.service.js
+++ src/services/usuario.service.js
@@ -1,4 +1,5 @@
 import * as usuarioDao from '../daos/usuario.dao.js';
+import bcrypt from 'bcryptjs';
 
 export const getUsuarios = async () => {
     return await usuarioDao.getUsuarios();
@@ -15,7 +16,16 @@ export const getUsuarioById = async (id) => {
 }
 
 export const createUsuario = async (usuario) => {
-    return await usuarioDao.createUsuario(usuario);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(usuario.password, salt)
+
+    const nuevoUsuario = {
+        ...usuario,
+        password: hashedPassword
+    };
+
+    return await usuarioDao.createUsuario(nuevoUsuario);
 }
 
 export const updateUsuario = async (id, usuarioData) => {
